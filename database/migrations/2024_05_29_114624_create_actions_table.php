8f66b74871be88da88cc8d937402812950b8344d
71414@hoornbeeck.nl <71414@hoornbeeck.nl>
2024-05-29 13:51:12 +0200

    Create actions table migration

---
+++ database/migrations/2024_05_29_114624_create_actions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('action_event', function (Blueprint $table){
+            $table->id();
+            $table->string('name', 20)->unique();
+        });
+        Schema::create('action_log', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('event_id')->unsigned();
+            $table->foreign('event_id')->references('id')->on('action_event');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('context', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('action_event');
+        Schema::dropIfExists('action_log');
+    }
+};
