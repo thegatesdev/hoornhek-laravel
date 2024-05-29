5f8704355ddaec937486d7a87b2aa94c77350b7a
71414@hoornbeeck.nl <71414@hoornbeeck.nl>
2024-05-29 13:01:44 +0200

    Create visits table migration

---
+++ database/migrations/2024_05_29_105456_create_visits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 254);
+            $table->bigInteger('prisoner_id')->unsigned();
+            $table->foreign('prisoner_id')->references('id')->on('prisoners');
+            $table->dateTime('visit_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
