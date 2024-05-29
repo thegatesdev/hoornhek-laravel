7bd69b41032c24e240e73cfa18919b16d5521923
71414@hoornbeeck.nl <71414@hoornbeeck.nl>
2024-05-29 11:53:44 +0200

    Create prisoners table migration

---
+++ database/migrations/2024_05_29_094455_create_prisoners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Psy\Shell;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prisoners', function (Blueprint $table) {
+            $table->id();
+            $table->string('bsn', 9)->unique();
+            $table->string('name', 50)->unique();
+            $table->string('address', 50)->unique();
+            $table->string('city', 50);
+            $table->timestamps();
+        });
+        Schema::create('cells', function (Blueprint $table){
+            $table->id('number');
+            $table->bigInteger('prisoner_id')->unsigned()->nullable();
+            $table->foreign('prisoner_id')->references('id')->on('prisoners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prisoners');
+        Schema::dropIfExists('cells');
+    }
+};
